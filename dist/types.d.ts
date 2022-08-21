e29b27b55d5163e26da0e22f7bf4c0355e5337ef
Ian Lucas <ianlucas_ferreira@hotmail.com>
2022-08-21 16:16:06 -0300

    build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,21 +1,21 @@
 import { routeHandler } from "@ianlucas/http";
 import * as express from "express";
-interface ISteamSpec {
+interface SteamSpec {
     apiKey: string;
-    onAuthenticate?: (user: IUserData) => Promise<void>;
+    onAuthenticate?: (user: UserData) => Promise<void>;
     onFail: routeHandler;
     onSuccess: routeHandler;
     realm: string;
     sessionPath: string;
     sessionSecret: string;
 }
-interface IUserData {
+export interface UserData {
     id: string;
     id2: string;
     name: string;
     photo: string;
 }
-export function useUserData(handler: (userData: IUserData) => Promise<void>): (userData: IUserData) => Promise<void>;
-export default function useSteamOAuth(spec: ISteamSpec): (app: express.Application) => void;
+export function useUserData(handler: (userData: UserData) => Promise<void>): (userData: UserData) => Promise<void>;
+export default function useSteamOAuth(spec: SteamSpec): (app: express.Application) => void;
 
 //# sourceMappingURL=types.d.ts.map
